36217c1aee1fb3a5d28f1559c19e4d163f6fd63e
Everone Graham <grahameverone@gmail.com>
2023-05-27 19:47:07 -0700

    More tweaks.

--- app/layout.js
+++ app/layout.js
@@ -1,16 +1,10 @@
 import React from "react";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
 import "./globals.css"
 
 export default function RootLayout({ children }) {
     return (
       <body lang="eng">
-        <div className="text-black bg-black flex flex-col min-h-screen">
-          <Header />
-            <body>{children}</body>
-          <Footer />
-        </div>
+        <body>{children}</body>
       </body>
     );
   }
