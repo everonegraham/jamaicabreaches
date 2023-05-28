aa544cc4fbcdb1c3e87d44e032a52c11fdb774e7
Everone Graham <grahameverone@gmail.com>
2023-05-27 19:51:00 -0700

    Making tweaks to fix strange vercel tailwindcss glitch.

--- app/page.js
+++ app/page.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Table from "../components/Table";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 export const metadata = {
     title: 'Jamaica Breaches',
@@ -14,18 +16,21 @@ export const metadata = {
 export default function Home() {
 
   return (
-
-    <section className="text-gray-600 body-font flex-grow">
-        <div className="max-w-5xl pt-52 pb-24 mx-auto">
-            <h1 className="text-80 text-center font-4 lh-6 ld-04 font-bold text-white mb-6">
-            The Jamaican Security Logbook
-            </h1>
-            <h2 className="text-2xl font-4 font-semibold lh-6 ld-04 pb-11 text-gray-700 text-center">
-            Recording security breaches, data leaks, hacks and scams taking place in Jamaica.
-            </h2>
-            <Table/>
-        </div>
-    </section>
+    <div className="text-black bg-black flex flex-col min-h-screen">
+      <Header />
+      <section className="text-gray-600 body-font flex-grow">
+          <div className="max-w-5xl pt-52 pb-24 mx-auto">
+              <h1 className="text-80 text-center font-4 lh-6 ld-04 font-bold text-white mb-6">
+              The Jamaican Security Logbook
+              </h1>
+              <h2 className="text-2xl font-4 font-semibold lh-6 ld-04 pb-11 text-gray-700 text-center">
+              Recording security breaches, data leaks, hacks and scams taking place in Jamaica.
+              </h2>
+              <Table/>
+          </div>
+      </section>
+      <Footer />
+    </div>
 
   );
 }
