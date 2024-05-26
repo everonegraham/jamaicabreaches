5b34cb17c8ac51bd64b9eedf6f6e13ddc8129008
Everone Graham <grahameverone@gmail.com>
2024-05-26 13:45:25 -0700

    Changes to the nav bar.

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import Link from "next/link"
+import { usePathname } from "next/navigation"
 
 import { siteConfig } from "@/config/site"
 import { buttonVariants } from "@/components/ui/button"
@@ -15,37 +16,42 @@ import React from "react"
 
 export function SiteHeader() {
   const [showMobileMenu, setShowMobileMenu] = React.useState<boolean>(false)
+  const pathname = usePathname()
   
   return (
     <header className="sticky top-0 z-40 w-full border-b bg-background">
       <div className="container flex w-full items-center justify-between p-4">
 
-        {/* Nav Start */}
-        <div className="mr-4 md:flex">
-          <Link href="/" className="mr-6 flex items-center space-x-2" passHref>
+        {/* Logo */}
+        <div className="flex items-center space-x-4">
+          <Link href="/" className="flex items-center space-x-2" passHref>
             <span className="font-bold sm:inline-block">{siteConfig.name}</span>
           </Link>
-          {siteConfig.mainNav?.length ? (
-            <nav className="hidden items-center space-x-6 text-sm font-medium md:flex">
-              {siteConfig.mainNav?.map(
-                (item, index) =>
-                  item.href && (
-                    <Link
-                      key={index}
-                      href={item.href}
-                      className={cn(
-                        "flex items-center text-sm font-medium text-muted-foreground"
-                      )}
-                    >
-                      {item.title}
-                    </Link>
-                  )
-              )}
-            </nav>
-          ) : null}
         </div>
 
-        <div className="hidden flex-1 items-center justify-end space-x-4 md:flex">
+        {/* Nav */}
+        <div className="z-10 flex grow justify-center">
+          <nav className="hidden items-center space-x-6 text-sm font-medium md:flex">
+            {siteConfig.mainNav?.map(
+              (item, index) =>
+                item.href && (
+                  <Link
+                    key={index}
+                    href={item.href}
+                    className={cn(
+                      "flex items-center rounded p-2 text-sm font-medium hover:bg-accent hover:text-accent-foreground",
+                      pathname === item.href ? "text-foreground" : "text-muted-foreground"
+                    )}
+                  >
+                    {item.title}
+                  </Link>
+                )
+            )}
+          </nav>
+        </div>
+        
+        {/* Social Icons */}
+        <div className="hidden items-center space-x-4 md:flex">
           <nav className="flex items-center space-x-1">
             <Link
               href={siteConfig.links.github}
@@ -82,82 +88,85 @@ export function SiteHeader() {
             <ThemeToggle />
           </nav>
         </div>
-        {/* Nav Ends */}
         
-        {/* Mobile Nav Start */}
+        {/* Mobile Button */}
         <button
           className="flex items-center space-x-2 md:hidden"
           onClick={() => setShowMobileMenu(!showMobileMenu)}
         >
           {showMobileMenu ? <Close /> : <Menu />}
         </button>
-        {showMobileMenu && (
-          <div
-            className={cn(
-              "fixed inset-0 top-16 z-50 grid grid-flow-row auto-rows-max overflow-auto p-6 shadow-md animate-in slide-in-from-bottom-80 md:hidden"
-            )}
-          >
-            <div className="relative z-20 grid gap-6 rounded-md bg-popover p-4 text-popover-foreground shadow-md">
-      
-              <nav className="grid grid-flow-row auto-rows-max text-sm">
-              {siteConfig.mainNav?.map(
-                    (item, index) =>
-                      item.href && (
-                  <Link
-                    key={index}
-                    href={item.href}
-                    className={cn(
-                      "flex w-full items-center rounded-md p-2 text-sm font-medium hover:underline"
-                    )}
-                    onClick={() => setShowMobileMenu(!showMobileMenu)}
-                  >
-                    {item.title}
-                  </Link>
-                ))}
-                <Separator className="my-2 mt-6" />
-                <div className="justfiy-between flex items-center">
-                  
-                  <Link
-                    href={siteConfig.links.github}
-                    target="_blank"
-                    rel="noreferrer"
-                    passHref
-                  >
-                    <div
-                      className={buttonVariants({
-                        size: "sm",
-                        variant: "ghost",
-                      })}
-                    >
-                      <Icons.gitHub className="h-5 w-5" />
-                      <span className="sr-only">GitHub</span>
-                    </div>
-                  </Link>
-                  <Link
-                    href={siteConfig.links.twitter}
-                    target="_blank"
-                    rel="noreferrer"
-                    passHref
-                  >
-                    <div
-                      className={buttonVariants({
-                        size: "sm",
-                        variant: "ghost",
-                      })}
-                    >
-                      <Icons.twitter className="h-5 w-5 fill-current" />
-                      <span className="sr-only">Twitter</span>
-                    </div>
-                  </Link>
-                  <ThemeToggle />
-                </div>
-              </nav>
-            </div>
-          </div>
-        )}
-
-        {/* Mobile Nav End */}
       </div>
+
+      {showMobileMenu && (
+        <div
+          className={cn(
+            "absolute inset-x-0 top-full z-50 flex flex-col items-center justify-center bg-background p-6 shadow-md md:hidden"
+          )}
+        >
+          <div className="relative z-20 flex w-full max-w-md flex-col items-center justify-center gap-6 rounded-md bg-popover p-4 text-popover-foreground shadow-md">
+    
+            <nav className="flex flex-col items-center justify-center text-sm">
+              {siteConfig.mainNav?.map((item, index) => {
+                if (item.href) {
+                  return (
+                    <Link
+                      key={index}
+                      href={item.href}
+                      className={cn(
+                        "flex w-full items-center justify-center rounded-md p-2 text-sm font-medium hover:underline",
+                        pathname === item.href ? "text-foreground" : "text-muted-foreground"
+                      )}
+                      onClick={() => setShowMobileMenu(!showMobileMenu)}
+                    >
+                      {item.title}
+                    </Link>
+                  );
+                }
+                return null;
+              })}
+              <Separator className="my-2 mt-6" />
+              <div className="flex items-center justify-between">
+                
+                <Link
+                  href={siteConfig.links.github}
+                  target="_blank"
+                  rel="noreferrer"
+                  passHref
+                >
+                  <div
+                    className={buttonVariants({
+                      size: "sm",
+                      variant: "ghost",
+                    })}
+                  >
+                    <Icons.gitHub className="h-5 w-5" />
+                    <span className="sr-only">GitHub</span>
+                  </div>
+                </Link>
+                <Link
+                  href={siteConfig.links.twitter}
+                  target="_blank"
+                  rel="noreferrer"
+                  passHref
+                >
+                  <div
+                    className={buttonVariants({
+                      size: "sm",
+                      variant: "ghost",
+                    })}
+                  >
+                    <Icons.twitter className="h-5 w-5 fill-current" />
+                    <span className="sr-only">Twitter</span>
+                  </div>
+                </Link>
+                <ThemeToggle />
+              </div>
+            </nav>
+          </div>
+        </div>
+      )}
+
     </header>
   )
 }
